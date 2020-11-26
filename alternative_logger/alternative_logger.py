0069f99abdee2891d80eea161b689044567246c6
Cap-Leekai <antondel00@gmail.com>
2020-11-26 15:34:14 +0300

    ed

--- alternative_logger/alternative_logger.py
+++ alternative_logger/alternative_logger.py
@@ -1,25 +1,29 @@
 import rclpy
+import time
+
 from rclpy.node import Node
-from marker_msgs.msg import *
-from rclpy import logging
 from rclpy.qos import qos_profile_sensor_data
+
+from marker_msgs.msg import *
 from sensor_msgs.msg import Imu
 
 rtp_id = 1
 
 class Topic_example(Node):
-    def __init__(self, topic_name, type):
+    def __init__(self, topic_name, type, data_time):
         self.topic_name = topic_name
         self.type = type
+        self.data_time = data_time
         print("topic %s init witch type %s" % (self.topic_name, self.type))
+
         self.topic_name_copy = self.topic_name
         # print("topic_name: ", self.topic_name)
         self.topic_name_copy = self.topic_name_copy.replace('/', '_')
         # print("as", self.topic_name_copy)
 
-    def write_file(self, data, file_name):
-        file = open('{0}.txt'.format(file_name), 'a')
-        file.write('\n{0}'.format("<========================================================>"))
+    def write_file(self, data, file_name, time_msg):
+        file = open('{0}.txt'.format(self.data_time + file_name), 'a')
+        file.write('\n{0}'.format("<--" + time_msg + "-->"))
         file.write('\n{0}'.format(data))
         file.close()
 
@@ -29,20 +33,31 @@ class Topic_example(Node):
         :return: nothing
         cb. is writing msg to file
         """
-        pass
-        self.write_file(msg, self.topic_name_copy)
-        # print(msg)
-        # self.get_logger().log(msg, Lo)
+        dynamic_time = time.localtime(time.time())
+
+        str_time_for_msg = str(dynamic_time.tm_mday) + "." + str(dynamic_time.tm_mon) + "." + str(
+            dynamic_time.tm_year) + "-" + str(dynamic_time.tm_hour) + ":" + str(dynamic_time.tm_min) + ":" + str(
+            dynamic_time.tm_sec)
+
+        print(dir(msg))
+
+        self.write_file(msg, self.topic_name_copy, str_time_for_msg)
+
 
 class alternative_logger(Node):
     def __init__(self, node_name):
         self.node_name = node_name
         super().__init__(self.node_name)
         self.get_logger().info("init node with name: %s" % self.node_name)
+        staric_time = time.localtime(time.time())
+
+        str_time_for_name_file = str(staric_time.tm_mday) + "." + str(staric_time.tm_mon) + "." + str(staric_time.tm_year) + "-" + str(staric_time.tm_hour) + ":" + str(staric_time.tm_min) + ":" + str(staric_time.tm_sec)
+
+        print("Time: " + str_time_for_name_file)
 
         #initing topic examples
-        topic1 = Topic_example("/rtp_%s/local_nav" % rtp_id, LocalNav)
-        topic2 = Topic_example("/rtp_%s/imu" % rtp_id, Imu)
+        topic1 = Topic_example("/rtp_%s/local_nav" % rtp_id, LocalNav, str_time_for_name_file)
+        topic2 = Topic_example("/rtp_%s/imu" % rtp_id, Imu, str_time_for_name_file)
 
         #initing subscriptions
         self.sub1 = self.create_subscription(topic1.type, topic1.topic_name, topic1.topic_cb, qos_profile_sensor_data)
