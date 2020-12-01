1e77d8f85b56f24c00baa3c742fedb824fb3a7ed
Cap-Leekai <antondel00@gmail.com>
2020-12-01 22:59:43 +0300

    add dynamic subscriber and dynamic importer topic-msges

--- alternative_logger/alternative_logger.py
+++ alternative_logger/alternative_logger.py
@@ -1,34 +1,42 @@
 #coding=utf8
 import rclpy
 import time
+import os
 
+from rosidl_runtime_py import import_message_from_namespaced_type, utilities
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
+#from ros2topic.api import get_msg_class
 
-from marker_msgs.msg import *
-from sensor_msgs.msg import Imu
 
 rtp_id = 1
 
+
 class Topic_example(Node):
-    def __init__(self, topic_name, type, data_time):
+    def __init__(self, topic_name, type, static_data_time):
         self.topic_name = topic_name
         self.type = type
-        self.data_time = data_time
+        self.static_data_time = static_data_time
         print("topic %s init witch type %s" % (self.topic_name, self.type))
 
-        self.topic_name_copy = self.topic_name
+        self._topic_name_copy = self.topic_name
         # print("topic_name: ", self.topic_name)
-        self.topic_name_copy = self.topic_name_copy.replace('/', '_')
+        self._topic_name_copy = self._topic_name_copy.replace('/', '_')
         # print("as", self.topic_name_copy)
 
-    def write_file(self, data, file_name, time_msg):
-        file = open('{0}.txt'.format(self.data_time + file_name), 'a')
+    def _write_file(self, data, file_name, time_msg):
+
+        if not os.path.isdir(str(self.static_data_time)):
+            os.mkdir(str(self.static_data_time))
+
+        # print(os.getcwd() + "/" + str(self.static_data_time))
+
+        file = open('{0}/{1}.txt'.format(os.getcwd() + "/" + str(self.static_data_time), self.static_data_time + file_name), 'a')
         file.write('\n{0}'.format("<--" + time_msg + "-->"))
         file.write('\n{0}'.format(data))
         file.close()
 
-    def topic_cb(self, msg):
+    def _topic_cb(self, msg):
         """
         :param msg:
         :return: nothing
@@ -36,13 +44,13 @@ class Topic_example(Node):
         """
         dynamic_time = time.localtime(time.time())
 
-        str_time_for_msg = str(dynamic_time.tm_mday) + "." + str(dynamic_time.tm_mon) + "." + str(
+        dynamic_str_time_for_msg = str(dynamic_time.tm_mday) + "." + str(dynamic_time.tm_mon) + "." + str(
             dynamic_time.tm_year) + "-" + str(dynamic_time.tm_hour) + ":" + str(dynamic_time.tm_min) + ":" + str(
             dynamic_time.tm_sec)
 
-        print(dir(msg))
+        # print(dir(msg))
 
-        self.write_file(msg, self.topic_name_copy, str_time_for_msg)
+        self._write_file(msg, self._topic_name_copy, dynamic_str_time_for_msg)
 
 
 class alternative_logger(Node):
@@ -56,27 +64,22 @@ class alternative_logger(Node):
 
         print("Time: " + str_time_for_name_file)
 
-        #initing topic examples
-        topic1 = Topic_example("/rtp_%s/local_nav" % rtp_id, LocalNav, str_time_for_name_file)
-        topic2 = Topic_example("/rtp_%s/imu" % rtp_id, Imu, str_time_for_name_file)
+        # получаем список существующих топиков и их типов
+        self.list_topics = self.get_topic_names_and_types()
+        # print(self.list_topics)
 
+        # creating topic objects
+        topic_objects_list = []
+        for topic in self.list_topics:
+            topic_objects_list.append(Topic_example(topic[0], utilities.get_message(topic[1][0]), str_time_for_name_file))  # utilities.get_message(topic[1][0]): позволяет импортировать сообщение в код получая на вход его строковое название
 
-        """
-        concept:
-        # Вместо инициализации топиков через кучу строк лучше будет сделать цикл for, 
-        который будет перебирать список топиков и типов сообщений, для каждого топика соответственно, и уже в этом цикле делать создание сабскрайбера.
-        Это позволит сделать инициализацию сабскрайберов динамическим и тогда программа будет подписываться на столько топиков, сколько указано в списке.
-        
-        for topic_and_type in topics_list:
-            create subscriptions for topic_and_type
-        
-        """
+        # print(topic_list)
 
         #initing subscriptions
-        self.sub1 = self.create_subscription(topic1.type, topic1.topic_name, topic1.topic_cb, qos_profile_sensor_data)
-        self.sub1
-        self.sub2 = self.create_subscription(topic2.type, topic2.topic_name, topic2.topic_cb, qos_profile_sensor_data)
-        self.sub2
+        for topic in topic_objects_list:
+            self.sub = self.create_subscription(topic.type, topic.topic_name, topic._topic_cb, qos_profile_sensor_data)
+            self.sub
+
 
 def main():
     rclpy.init()
